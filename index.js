35597a9a93b1650fc808d5699eea80cf1e9d7eab
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-10-08 16:04:51 +0200

    added jwt

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -22,7 +24,22 @@ const client = new MongoClient(uri, {
 		deprecationErrors: true,
 	},
 });
-
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res.status(401).send({ error: true, message: 'unauthorized access' });
+	}
+	const token = authorization.split(' ')[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res.status(402).send({ error: true, message: 'unauthorized access' });
+		}
+		//req a amra ekta property boshacchi as value decoded(parameter)
+		req.decoded = decoded;
+		console.log(decoded);
+		next();
+	});
+};
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +47,12 @@ async function run() {
 		const database = client.db('carsDoctor');
 		const serviceCollection = database.collection('services');
 		const bookingCollection = database.collection('bookings');
+		//jwt-login er por ai khane hit korbe
+		app.post('/jwt', (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+			res.send({ token });
+		});
 
 		//i am taking data from database and making them available in this url
 		app.get('/services', async (req, res) => {
@@ -49,8 +72,14 @@ async function run() {
 			res.send(result);
 		});
 		// bookings
-		app.get('/bookings', async (req, res) => {
-			console.log(req.query.email);
+		app.get('/bookings', verifyJWT, async (req, res) => {
+			//jwt token ta pacchi
+			// console.log(req.headers);
+			const decoded = req.decoded;
+			console.log(decoded);
+			if (decoded.email !== req.query.email) {
+				return res.status(403).send({ error: 1, message: 'forbidden access' });
+			}
 			let query = {};
 			if (req.query?.email) {
 				query = { email: req.query.email };
@@ -88,6 +117,7 @@ async function run() {
 
 		// some data
 		app.get('/bookings', async (req, res) => {
+			console.log(req.headers);
 			const query = {};
 			if (req.query?.email) {
 				query = { email: req.query.email };
@@ -95,6 +125,7 @@ async function run() {
 			const result = await bookingCollection.find().toArray();
 			res.send(result);
 		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
